2abc1baa9ab5ce4b99d7c4ecd90a8356367fc516
Philippe <audiophil157@gmail.com>
2022-11-03 00:03:32 +0100

    Put all the polybar palettes in a file. Added Dunst config.

--- i3/setrdbg.sh
+++ i3/setrdbg.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+feh --bg-fill /Storage/Pictures/Pictures/Gazing\ the\ Clouds.png
+
 #while :
 #	do
-#feh --randomize --bg-fill /Storage/Pictures/Wallpapers/
-feh --bg-fill /Storage/Pictures/Pictures/Gazing\ the\ Clouds.png
+#		feh --randomize --bg-fill /Storage/Pictures/Wallpapers/
 #		sleep 300
 #	done
